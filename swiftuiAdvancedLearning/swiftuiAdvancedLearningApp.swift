dbe8befc9e9ad33716106cbbd2622e2b25159504
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-18 11:29:39 +0100

    Initial Commit

---
+++ swiftuiAdvancedLearning/swiftuiAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiAdvancedLearningApp.swift
+//  swiftuiAdvancedLearning
+//
+//  Created by MacBook on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
