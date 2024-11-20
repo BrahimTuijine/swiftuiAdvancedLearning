47049bd2699568455479121fec808e7a58707a66
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-20 10:11:20 +0100

    draw ShapeWithArc

--- swiftuiAdvancedLearning/CustomCurveScreen.swift
+++ swiftuiAdvancedLearning/CustomCurveScreen.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct CustomCurveScreen: View {
     var body: some View {
-        ArcSimple()
-            .stroke(style: StrokeStyle(lineWidth: 5))
+        ShapeWithArc()
+//            .stroke(style: StrokeStyle(lineWidth: 5))
             .frame(width: 200, height: 200)
     }
 }
 
 #Preview {
     CustomCurveScreen()
+}
+
+struct ShapeWithArc : Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false
+            )
+//            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        
+        }
+    }
 }
 
 struct ArcSimple : Shape {
