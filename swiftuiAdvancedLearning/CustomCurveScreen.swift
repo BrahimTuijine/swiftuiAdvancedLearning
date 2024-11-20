70c0535303fa9a963d051154ebdad22f39405b56
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-20 11:11:21 +0100

    draw WaterShape

--- swiftuiAdvancedLearning/CustomCurveScreen.swift
+++ swiftuiAdvancedLearning/CustomCurveScreen.swift
@@ -9,14 +9,40 @@ import SwiftUI
 
 struct CustomCurveScreen: View {
     var body: some View {
-        QuadSimple()
+        WaterShape()
+            .fill(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.blue, Color.blue]),
+                    startPoint: .leading,
+                    endPoint: .trailing)
+            )
+            .ignoresSafeArea()
 //            .stroke(style: StrokeStyle(lineWidth: 5))
-            .frame(width: 200, height: 200)
+//            .frame(width: 200, height: 200)
     }
 }
 
 #Preview {
     CustomCurveScreen()
+}
+
+struct WaterShape : Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+            
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.40))
+            
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.60))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
 }
 
 struct QuadSimple : Shape {
