694ce6b60390a397adddb1d4d17393a67d699c20
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-11-24 16:09:12 +0100

    animated tabs background using matchedGeometryEffect

--- swiftuiAdvancedLearning/CustomTabBar/CustomTabBarView.swift
+++ swiftuiAdvancedLearning/CustomTabBar/CustomTabBarView.swift
@@ -12,37 +12,31 @@ struct CustomTabBarView: View {
     
     let tabs: [TabBarItem]
     @Binding var selectedItem: TabBarItem
-//        .matchedGeometryEffect(id: "tabBarItem", in: nameSpace)
+       
+    @Namespace private var nameSpace
     
     var body: some View {
-        HStack {
-            ForEach(tabs, id: \.self) { item in
-                tabBarItem(item: item)
-                    
-            }
-        }
-        .padding(6)
-        .background(Color.white)
-        .ignoresSafeArea()
+        tabBarVersion2
     }
 }
+
+
+
+private let tabs: [TabBarItem] = [
+    .home, .favorites, .profile
+]
 
 #Preview {
     
     VStack {
-        
-        let tabs: [TabBarItem] = [
-            .home, .favorites, .profile
-        ]
         Spacer()
-        
         CustomTabBarView(tabs: tabs, selectedItem: .constant(tabs.first!))
     }
 }
 
 extension CustomTabBarView {
     
-    private func tabBarItem(item: TabBarItem) -> some View {
+    private func tabBarView(item: TabBarItem) -> some View {
         VStack {
             Image(systemName: item.iconName)
                 .font(.headline)
@@ -59,12 +53,65 @@ extension CustomTabBarView {
                 
             
         )
-//        .cornerRadius(10)
         .onTapGesture {
             withAnimation(.easeInOut) {
                 selectedItem = item
             }
         }
     }
+    
+    private var tabBarVersion1 : some View {
+        HStack {
+            ForEach(tabs, id: \.self) { item in
+                tabBarView(item: item)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .ignoresSafeArea()
+    }
+    
+    private func tabBarView2(item: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: item.iconName)
+                .font(.headline)
+            
+            Text(item.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .foregroundColor(selectedItem == item ? item.color : .gray)
+        .background(
+            ZStack {
+                if selectedItem == item {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(item.color.opacity(0.2))
+                        .matchedGeometryEffect(id: "tabBarItem", in: nameSpace)
+                }
+            }
+
+        )
+        .onTapGesture {
+            withAnimation(.easeInOut) {
+                selectedItem = item
+            }
+        }
+    }
+    
+    private var tabBarVersion2: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { item in
+                tabBarView2(item: item)
+                    
+            }
+        }
+        .padding(6)
+        .background(Color.white.ignoresSafeArea(edges: .bottom))
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 10, x:0, y: 5)
+        .padding(.horizontal)
+    }
+     
 }
 
