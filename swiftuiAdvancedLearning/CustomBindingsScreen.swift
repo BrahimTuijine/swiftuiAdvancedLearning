ec838536e98bab7683006bf4fbae69784ac8e6eb
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-02 11:33:56 +0100

    create custom binding with alert

--- swiftuiAdvancedLearning/CustomBindingsScreen.swift
+++ swiftuiAdvancedLearning/CustomBindingsScreen.swift
@@ -11,14 +11,47 @@ struct CustomBindingsScreen: View {
     
     @State private var title: String = "start"
     
+    @State private var errorTitle: String?
+//    @State private var showError: Bool = false
+    
     var body: some View {
-        Text(title)
-        
-        ChildView(title: $title)
-        ChildView2(title: title) { text in
-            title = text
+        VStack {
+            Text(title)
+            
+            ChildView(title: $title)
+            ChildView2(title: title) { text in
+                title = text
+            }
+            ChildView3(title: $title)
+            ChildView3(title: Binding(get: {
+                return title
+            }, set: { newValue in
+                title = newValue
+            }))
+            
+            Button("click me") {
+                errorTitle = "new Error !!"
+//                showError.toggle()
+            }
         }
+        .alert(errorTitle ?? "this is error", isPresented: Binding(get: {
+           return errorTitle != nil
+        }, set: { newValue in
+            if !newValue {
+                errorTitle = nil
+            }
+        })) {
+            Button("Ok") {
+               
+            }
+        }
+//        .alert(errorTitle ?? "this is error", isPresented: $showError) {
+//            Button("Ok") {
+//                
+//            }
+//        }
     }
+        
 }
 
 struct ChildView : View {
@@ -43,8 +76,19 @@ struct ChildView2 : View {
         Text("value  \(title)")
            
         Button("change title") {
-            setTitle("new value")
+            setTitle("new Title 2")
         }
+    }
+}
+
+struct ChildView3 : View {
+    let title: Binding<String>
+    
+    var body: some View {
+        Text("\(title.wrappedValue)")
+            .onAppear {
+                title.wrappedValue = "new Title 3"
+            }
     }
 }
 
