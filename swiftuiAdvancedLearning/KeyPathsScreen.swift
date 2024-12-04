0342425172ca2e8bbbdcd728799445c82b786665
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-04 16:30:05 +0100

    create custom sortByKeyPath and sortedByKeyPath

--- swiftuiAdvancedLearning/KeyPathsScreen.swift
+++ swiftuiAdvancedLearning/KeyPathsScreen.swift
@@ -7,26 +7,72 @@
 
 import SwiftUI
 
-struct MyDataModel {
+struct MyDataModel : Identifiable {
     let id = UUID().uuidString
     let title: String
     let date: Date
+    let count: Int
+}
+
+extension Array {
+    
+   // sort the array it self
+   mutating func sortByKeyPath<T: Comparable>(_ keyPath: KeyPath<Element, T>, ascending: Bool = true) {
+        self.sort(by: {
+            let value1 = $0[keyPath: keyPath]
+            let value2 = $1[keyPath: keyPath]
+            
+            return ascending ? value1 < value2 : value1 > value2
+            
+        })
+    }
+    
+    
+    
+    // return new sorted array
+    func sortedByKeyPath<T: Comparable>(_ keyPath: KeyPath<Element, T>, ascending: Bool = true) -> [Element] {
+        self.sorted(by: {
+            let value1 = $0[keyPath: keyPath]
+            let value2 = $1[keyPath: keyPath]
+            
+            return ascending ? value1 < value2 : value1 > value2
+            
+        })
+    }
 }
 
 struct KeyPathsScreen: View {
     
     @State private var screentitle: String = ""
+    @State private var dataArray: [MyDataModel] = []
     
     var body: some View {
-        Text(screentitle)
+        List {
+            ForEach(dataArray) { data in
+                VStack(alignment: .leading) {
+                    Text("\(data.count)")
+                    Text(data.title)
+                    Text(data.date.description)
+                }
+                .font(.headline)
+            }
+        }
             .onAppear {
+                let array = [
+                    MyDataModel(title: "Three", date: .distantFuture, count: 3),
+                    MyDataModel(title: "One", date: .now, count: 1),
+                    MyDataModel(title: "Two", date: .distantPast , count: 2),
+                ]
                 
-                let item = MyDataModel(title: "One", date: .now)
+//                let sortedArray = array.sorted(by: {$0.count < $1.count})
                 
-//                let title = item.title
-                let title = item[keyPath: \.title]
+                //using keyPaths
+//                let sortedArray = array.sorted(by: {$0[keyPath: \.count] < $1[keyPath: \.count]})
                 
-                screentitle = title
+                let sortedArray = array.sortedByKeyPath(\.count, ascending: false)
+            
+                
+                dataArray = sortedArray
             }
     }
 }
